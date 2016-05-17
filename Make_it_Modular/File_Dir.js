ad39acacd91718f3192cf4b407595a24b6d096e6
Alex <johnalexmarich@gmail.com>
2016-05-17 06:31:42 +0000

    Completed Make it Modular Task

---
+++ Make_it_Modular/File_Dir.js
@@ -0,0 +1,19 @@
+module.exports = 
+ {
+     getList: function(dir, extension, callback) {
+        var fs = require('fs');
+        var list = [];
+        
+        
+        fs.readdir(dir, function(err, contents) {
+            if(err) return callback(err);
+           
+            for(var i = 0; i < contents.length; i++)
+                if(contents[i].indexOf("." + extension) > -1)
+                    list.push(contents[i]);
+            
+            
+            callback(null, list);
+        });
+    }
+};
---
+++ Make_it_Modular/Make_it_Modular.js
@@ -0,0 +1,17 @@
+
+if(process.argv.length > 3) {
+    
+    var mod = require("./File_Dir.js");
+    var dir = process.argv[2];
+    var filter = process.argv[3];
+    
+    mod.getList(dir, filter, function(err, contents) {
+        if(err) console.log(err);
+        
+        for(var i = 0; i < contents.length; i++)
+            console.log(contents[i]);
+        
+        
+    });
+
+}
